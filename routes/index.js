8cbbc3b2bf8b7147222c6ddf8f480b35259379be
rkelecava <rkelecava@scottelectricusa.com>
2016-05-23 15:30:31 -0400

    Added route to preload post and get a Single post

--- routes/index.js
+++ routes/index.js
@@ -23,6 +23,24 @@ router.post('/posts', function (req, res, next) {
 	});
 });
 
+/* PRELOAD "Post" Object */
+router.param('post', function (req, res, next, id) {
+	var query = Post.findById(id);
+
+	query.exec(function (err, post) {
+		if (err) { return next(err); }
+		if (!post) { return next(new Error('can\'t find post')); }
+
+		req.post = post;
+		return next();
+	});
+});
+
+/* GET (RETRIEVE) a Single Post */
+router.get('/posts/:post', function (req, res) {
+	res.json(req.post);
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
