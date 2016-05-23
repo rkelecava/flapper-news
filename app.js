73963d2a685cc66f195f254f9a968d1160b7653d
rkelecava <rkelecava@scottelectricusa.com>
2016-05-23 14:29:00 -0400

    Added Posts Model NodeJs

--- app.js
+++ app.js
@@ -5,6 +5,11 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+var mongoose = require('mongoose');
+require('./models/Posts');
+
+mongoose.connect('mongodb://localhost/flapperNews');
+
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
