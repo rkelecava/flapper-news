81c132f14f069c8c434bd55a667abe0a21d90b7b
rkelecava <rkelecava@scottelectricusa.com>
2016-05-23 10:25:03 -0400

    added ability to enter a post

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -8,4 +8,10 @@ app.controller('MainCtrl', ['$scope', function ($scope) {
 		{title: 'post 4', upvotes: 9},
 		{title: 'post 5', upvotes: 4}
 	];
+
+	$scope.addPost = function () {
+		if (!$scope.title || $scope.title === '') { return; }
+		$scope.posts.push({title: $scope.title, upvotes: 0});
+		$scope.title = '';
+	};
 }]);
