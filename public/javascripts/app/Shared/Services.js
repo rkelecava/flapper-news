b98416f4d7da3ad5a9f4a377f75072fdd690bb77
rkelecava <rkelecava@scottelectricusa.com>
2016-05-25 15:25:51 -0400

    Fixed and moved Post Author not displaying

--- public/javascripts/app/Shared/Services.js
+++ public/javascripts/app/Shared/Services.js
@@ -14,7 +14,7 @@ sharedServices.factory('posts', ['$http', 'auth', function ($http, auth) {
 
 	o.create = function (post) {
 		return $http.post('/posts', post, {
-			headers: {Authorization: 'Bearer '+auth.getToken()}
+			headers: {Authorization: 'Bearer '+ auth.getToken()}
 			})
 			.success(function (data) {
 				o.posts.push(data);
@@ -23,7 +23,7 @@ sharedServices.factory('posts', ['$http', 'auth', function ($http, auth) {
 
 	o.upvote = function (post) {
 		return $http.put('/posts/' + post._id + '/upvote', null, {
-			headers: {Authorization: 'Bearer '+auth.getToken()}
+			headers: {Authorization: 'Bearer '+ auth.getToken()}
 			})
 			.success(function (data) {
 				post.upvotes += 1;
@@ -39,13 +39,13 @@ sharedServices.factory('posts', ['$http', 'auth', function ($http, auth) {
 
 	o.addComment = function (id, comment) {
 		return $http.post('/posts/' + id + '/comments', comment, {
-			headers: {Authorization: 'Bearer '+auth.getToken()}
+			headers: {Authorization: 'Bearer ' + auth.getToken()}
 		});
 	};
 
 	o.upvoteComment = function(post, comment) {
 		return $http.put('/posts/' + post._id + '/comments/' + comment._id + '/upvote', null, {
-			headers: {Authorization: 'Bearer '+auth.getToken()}
+			headers: {Authorization: 'Bearer ' + auth.getToken()}
 			})
 			.success(function (data) {
 				comment.upvotes += 1;
